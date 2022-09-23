489aedb896f13e6daba35068bc873b3772b3f689
crossbell8368 <godfather346@gmail.com>
2022-09-23 13:26:00 +0900

    Initial Commit

---
+++ git_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git_test
+//
+//  Created by CrossBell on 2022/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
